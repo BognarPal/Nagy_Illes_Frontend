01ed557e41623b9c4e67032e199a0b3899f8f487
KondorArminAron23 <kondor.armin.aron@students.jedlik.eu>
2023-02-18 16:13:21 +0100

    Created Login/Register

--- src/app/Components/app.component.ts
+++ src/app/Components/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../Models/user';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -12,19 +14,19 @@ export class AppComponent implements OnInit {
   //Az user bármi lehet string, int stb
   users:any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(){
-    this.getUsers();
+    
+    this.setCurrentUser()
   }
 
-  getUsers(){
-    this.http.get('http://localhost:5241/api/Users').subscribe(response=>{
-      this.users= response;
-    }, error=>{
-      console.log(error);
-    })
+  setCurrentUser(){
+    const user: User= JSON.parse(localStorage.getItem('user') || '{}');
+    this.accountService.setCurrentUser(user);
   }
+
+  
 }
 
 
--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -1,15 +1,48 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, ReplaySubject } from 'rxjs';
+import { User } from '../Models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
  baseUrl= 'http://localhost:5241/api/';
+ private currentUserSource = new ReplaySubject<User>(1);
+ currentUser$ =this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
 
   login(model:any){
-    return this.http.post(this.baseUrl+'Users/login'+'?' + encodeURI(`email=${model.email}&password=${model.password}`), null);
+    return this.http.post<User>(this.baseUrl+'Users/login'+'?' + encodeURI(`email=${model.email}&password=${model.password}`), null).pipe(
+      map((response: User) =>{
+        const user = response;
+        if(user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  register(model:any){
+    return this.http.post(this.baseUrl+'Users/register'+'?' + encodeURI(`username=${model.username}&email=${model.email}&password=${model.password}`), null).pipe(
+      map((user: User)=>{
+        if (user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        
+      })
+    )
+  }
+
+  setCurrentUser(user: User){
+    this.currentUserSource.next(user);
+  }
+
+  logout(){
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null!);
   }
 }
