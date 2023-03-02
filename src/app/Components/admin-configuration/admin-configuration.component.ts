329e82ed5bb40b47806d37ae4c5f5e5e510fbcbf
KondorArminAron23 <kondor.armin.aron@students.jedlik.eu>
2023-03-02 11:18:28 +0100

    added toastr to admin-config

--- src/app/Components/admin-configuration/admin-configuration.component.ts
+++ src/app/Components/admin-configuration/admin-configuration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { ConfigModel } from 'src/app/models';
 import { ConfigurationService } from 'src/app/services/configuration.service';
 import { environment } from 'src/Enviroments/environment';
@@ -10,22 +11,34 @@ import { environment } from 'src/Enviroments/environment';
 })
 export class AdminConfigurationComponent {
   config: ConfigModel;
-  constructor(private configurationService: ConfigurationService) { }
-
+  constructor(private configurationService: ConfigurationService, private toastr: ToastrService) { }
   baseUrl = environment.apiUrl;
-
-
 
   ngOnInit() {
     this.configurationService.getConfig().subscribe(config => {
       this.config = config;
     });
   }
-  
+
+  /*
   onSave() {
     this.configurationService.updateConfig(this.config).subscribe(
       () => console.log('Configuration updated successfully'),
       error => console.error('Error updating configuration', error)
     );
   }
+  */
+
+  onSave() {
+    this.configurationService.updateConfig(this.config).subscribe(
+      () => {
+        this.toastr.success('Configuration updated successfully');
+        console.log('Configuration updated successfully');
+      },
+      error => {
+        this.toastr.error('Error updating configuration', error);
+        console.error('Error updating configuration', error);
+      }
+    );
+}
 }
