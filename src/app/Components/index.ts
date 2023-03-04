33b540d64653c9263c4bb691de93e69f19be8420
KondorArminAron23 <kondor.armin.aron@students.jedlik.eu>
2023-03-04 16:36:17 +0100

    Finished classcards

--- src/app/Components/index.ts
+++ src/app/Components/index.ts
@@ -7,3 +7,4 @@ export * from './app.component';
 export * from './profile-edit/profile-edit.component'
 export * from './users/user-list/user-list.component';
 export * from './admin-configuration/admin-configuration.component'
+export * from './classes/classes.component'
--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,2 +1,4 @@
 export * from './account.service';
 export * from './users.service';
+export * from './configuration.service';
+export * from './class.service';
